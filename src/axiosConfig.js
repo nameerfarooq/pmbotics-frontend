4ac4039fd1293829272e7defd9159a46c7ac12d3
Muhammad Nameer <61945782+nameerfarooq@users.noreply.github.com>
2024-08-16 12:23:39 +0500

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://pmbotics.herokuapp.com/",
+  baseURL: "https://usamaali.pythonanywhere.com/",
 });
 
 instance.interceptors.request.use(
